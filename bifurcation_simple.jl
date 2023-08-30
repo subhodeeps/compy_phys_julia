a7181914c65484c29445ab8f7f9220be0232e9a1
Subhodeep Sarkar <49411080+subhodeeps@users.noreply.github.com>
2023-08-31 01:56:54 +0530

    Update bifurcation_simple.jl
    removed colours

--- bifurcation_simple.jl
+++ bifurcation_simple.jl
@@ -5,30 +5,32 @@ function logistic(a, x)
     return a * x * (1 - x)
 end
 
-amin, amax, aN = 2.8, 4.0,2000
+amin, amax, aN = 2.8, 4.0, 2000
 as = range(amin, amax, length=aN) #2000
 itmax = 1000 # number of iterations for each a 
 ihide = 1000 # number of hidden iterations before plotting
 xtiny = 1e-10 # initial value for x
 y = []
 xx = []
-zc = [] # for colours
+# zc = [] # for colours
 for a in as
     x = xtiny
-    ic = 0 # for colours
+    # ic = 0 # for colours
     for i = 1:(ihide+itmax)
         x = logistic(a, x)
         if i > ihide
             push!(y, x)
             push!(xx, a)
-            ic = mod(ic, 50) + 1
-            push!(zc, ic)
+            # ic = mod(ic, 50) + 1
+            # push!(zc, ic)
         end
     end
 end
-da = (amax-amin)/aN
-p = scatter(xx, y, zcolor=zc, xlimits=(amin-2*da, amax+2*da), ylimits=(0, 1),
-    ms=0.2, msw=0, color=palette(:bluesreds, length(unique(zc))), legend=false, grid=false, framestyle=:box)
+da = (amax - amin) / aN
+# p = scatter(xx, y, zcolor=zc, xlimits=(amin-2*da, amax+2*da), ylimits=(0, 1),
+#     ms=0.2, msw=0, color=palette(:bluesreds, length(unique(zc))), legend=false, grid=false, framestyle=:box)
+p = scatter(xx, y, xlimits=(amin, amax), ylimits=(0, 1), mc=:black,
+    ms=0.2, msw=0, legend=false, grid=false, framestyle=:box)
 xlabel!("\$\\alpha\$") # instead of the L marco
 ylabel!(L"x_{n}")
 title!(L"x_{n}=\alpha x_{n-1}(1-x_{n-1})");
