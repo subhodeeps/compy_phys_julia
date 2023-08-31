862fb29e70fd577901358508d0017b08029486ae
Subhodeep Sarkar <49411080+subhodeeps@users.noreply.github.com>
2023-09-01 02:07:47 +0530

    Update bifurcation_binned.jl
    log plot

--- bifurcation_binned.jl
+++ bifurcation_binned.jl
@@ -1,23 +1,35 @@
+"""
+Generate a bifurcation diagram of the logistic equation
+
+Author: Subhodeep Sarkar
+Contact: subhodeep.sarkar1@gmail.com
+Date: 01-08-2023
+
+Note: Modify a_N, itMAX, nBINS to control the quality
+of the bifurcation diagram
+
+"""
+
 # import packages
 
 using Plots
 using LaTeXStrings
-using LoopVectorization
+using BenchmarkTools
 
 # define functions
 
-function logistic(a, x)
-    return a * x * (1 - x)
+function logistic(a_val, x_val)
+    return a_val * x_val * (1 - x_val)
 end
 
-function compute_data!(data, as, xtiny, ithide, itmax, nbins)
-    for (a_idx, a) in enumerate(as)
-        x = xtiny
+function compute_data(data, as, x0, ithide, itmax, nbins)
+    for a_idx in eachindex(as)
+        x = x0
         for i = 1:ithide
-            x = logistic(a, x)
+            x = logistic(as[a_idx], x)
         end
         for i = 1:itmax
-            x = logistic(a, x)
+            x = logistic(as[a_idx], x)
             index = ceil(Int, nbins * x)
             data[index, a_idx] += 1
         end
@@ -25,29 +37,30 @@ function compute_data!(data, as, xtiny, ithide, itmax, nbins)
     return data
 end
 
-# parameters and initialization
+@btime begin
 
-amin, amax, a_N = 2.8, 4.0, 8000 # limits of the x axis and number of points
-as = range(amin, amax, a_N) # range of a (x axis)
-itmax = 5000 # number of iterations for each a 
-ithide = 1000 # number of hidden iterations before plotting
-xtiny = 1e-10 # initial value for x
-nbins = 2000 # resolution along the y axis
-data = zeros(nbins, a_N) # array to store the computed data
+    # parameters and initialization
+    amin, amax, a_N = 2.8, 4.0, 10000 # limits of the x axis and number of points
+    as_vals = range(amin, amax, a_N) # range of a (x axis)
+    itMAX = 2500000 # number of iterations for each a 
+    itHIDE = 100000 # number of hidden iterations before plotting
+    xtiny = 1e-10 # initial value for x
+    nBINS = 3000 # resolution along the y axis
+    data_arr = zeros(nBINS, a_N) # array to store the computed data
 
-# main execution
-data = compute_data!(data, as, xtiny, ithide, itmax, nbins)
+    # main execution
+    data_arr = compute_data(data_arr, as_vals, xtiny, itHIDE, itMAX, nBINS)
+    data_arr ./= maximum(data_arr, dims=1) # normalization
 
-data ./= maximum(data, dims=1)
+    # plotting 
+    xmin, xmax = 0, 1 # range of x axis
+    xs = range(xmin, xmax, length=nBINS)
+    p = heatmap(as_vals, xs, log10.(data_arr), c=cgrad(:darktest, rev=false),
+        colorbar=false, grid=false, framestyle=:box)
+    xlabel!("\$\\alpha\$") # instead of the L marco
+    ylabel!(L"x_{n}")
+    title!(L"x_{n}=\alpha x_{n-1}(1-x_{n-1})")
+    savefig(p, "plots/bifurcation_binned.pdf")
 
-# plotting 
+end
 
-xmin, xmax = 0, 1 # range of x axis
-xs = range(xmin, xmax, length=nbins)
-
-p = heatmap(as, xs, data, c=cgrad(:grays, rev=true),
-    colorbar=false, grid=false)
-xlabel!("\$\\alpha\$") # instead of the L marco
-ylabel!(L"x_{n}")
-title!(L"x_{n}=\alpha x_{n-1}(1-x_{n-1})");
-savefig(p, "plots/bifurcation_binned.pdf")
